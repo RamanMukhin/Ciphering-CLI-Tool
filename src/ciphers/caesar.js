d17844487fc3dcff0fb1e1e482e00042637d10eb
Raman Mukhin <Kextcf2018@tut.by>
2021-11-09 15:12:41 +0300

    feat: added caesar ciepher

---
+++ src/ciphers/caesar.js
@@ -0,0 +1,28 @@
+import { CaesarKey } from "./caesarKey.js";
+
+export class Caesar {
+    constructor() {
+        this.key = new CaesarKey();
+    }
+
+    encode(string) {
+        const encoded = string.split('').map(simbol => {
+            return this.key.encode.hasOwnProperty(simbol)
+                ? this.key.encode[simbol]
+                : simbol;
+        });
+
+        return encoded.join('');
+    }
+
+    decode(string) {
+        const decoded = string.split('').map(simbol => {
+            return this.key.decode.hasOwnProperty(simbol)
+                ? this.key.decode[simbol]
+                : simbol;
+        });
+
+        return decoded.join('');
+    }
+
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+import { Caesar } from "./src/ciphers/caesar.js";
+
+const test = new Caesar();
+
+console.log('RESULT:   ', test.decode('ABCDE'));
